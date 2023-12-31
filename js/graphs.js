060a9bb811b4824783b3bc8cc9bf9fc6b1526f29
manohar322028 <manohardahal40@gmail.com>
2023-12-31 10:27:10 +0545

    added points, linegraph in curvebasis

--- js/graphs.js
+++ js/graphs.js
@@ -1,6 +1,29 @@
 import { loadData } from "./data.js";
+import { lineGraph } from "./linegraph.js";
 
-loadData().then(([featureData, streamData]) => {
-  console.log(featureData);
-  console.log(streamData);
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const graph = d3
+  .select("body")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
+
+loadData().then(([, streamData]) => {
+  const plot = lineGraph()
+    .width(width)
+    .height(height)
+    .data(streamData)
+    .xValue((d) => d["rank"])
+    .yValue((d) => d["Streams (Billions)"])
+    .margin({
+      top: 20,
+      right: 20,
+      bottom: 40,
+      left: 50,
+    })
+    .thickness(2);
+
+  graph.call(plot);
 });
--- js/linegraph.js
+++ js/linegraph.js
@@ -3,4 +3,101 @@ const extent = d3.extent;
 const axisLeft = d3.axisLeft;
 const axisBottom = d3.axisBottom;
 
-const linegraph = () => {};
+export const lineGraph = () => {
+  let width;
+  let height;
+  let data;
+  let xValue;
+  let yValue;
+  let margin;
+  let thickness;
+
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  const my = async (selection) => {
+    const x = scaleLinear()
+      .domain(extent(data, xValue))
+      .range([margin.left, width - margin.right]);
+
+    const y = scaleLinear()
+      .domain(extent(data, yValue))
+      .range([height - margin.bottom, margin.top]);
+
+    const marks = data.map((d) => ({
+      x: x(xValue(d)),
+      y: y(yValue(d)),
+    }));
+    await delay(2000);
+
+    const circles = selection
+      .selectAll("circle")
+      .data(marks)
+      .join("circle")
+      .attr("cx", (d) => d.x)
+      .attr("cy", (d) => d.y)
+      .attr("r", thickness * 1.5);
+
+    // console.log("marks:", marks);
+    selection
+      .selectAll("path")
+      .data([marks])
+      .join("path")
+      .attr(
+        "d",
+        d3
+          .line()
+          .x((d) => d.x)
+          .y((d) => d.y)
+          .curve(d3.curveBasis)
+      )
+      .attr("fill", "none")
+      .attr("stroke", "black")
+      .attr("stroke-width", thickness);
+
+    selection
+      .selectAll(".y-axis")
+      .data([null])
+      .join("g")
+      .attr("class", "y-axis")
+      .attr("transform", `translate(${margin.left},0)`)
+      .call(axisLeft(y));
+
+    selection
+      .selectAll(".x-axis")
+      .data([null])
+      .join("g")
+      .attr("class", "x-axis")
+      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(axisBottom(x));
+  };
+
+  my.width = function (_) {
+    return arguments.length ? ((width = +_), my) : width;
+  };
+
+  my.height = function (_) {
+    return arguments.length ? ((height = +_), my) : height;
+  };
+
+  my.data = function (_) {
+    return arguments.length ? ((data = _), my) : data;
+  };
+
+  my.xValue = function (_) {
+    return arguments.length ? ((xValue = _), my) : xValue;
+  };
+
+  my.yValue = function (_) {
+    return arguments.length ? ((yValue = _), my) : yValue;
+  };
+
+  my.margin = function (_) {
+    return arguments.length ? ((margin = _), my) : margin;
+  };
+
+  my.thickness = function (_) {
+    return arguments.length ? ((thickness = +_), my) : thickness;
+  };
+
+  return my;
+};
